7d14f6bb244f77b928229390c30b3f5d2e86d11c
Michal Kowalik <mkowalik@outlook.com>
2016-02-15 23:12:26 +0100

    adding second problem to the set

--- problemSet5.R
+++ problemSet5.R
@@ -21,3 +21,25 @@ ggplot(data = diamonds, aes(x = price, fill = cut)) +
     geom_histogram() +
     scale_fill_brewer(type = 'qual') +
     facet_wrap(~ color)
+
+# Create a scatterplot of diamond price vs.
+# table and color the points by the cut of
+# the diamond.
+
+# The plot should look something like this.
+# http://i.imgur.com/rQF9jQr.jpg
+
+# Note: In the link, a color palette of type
+# 'qual' was used to color the scatterplot using
+# scale_color_brewer(type = 'qual')
+
+# This assignment is not graded and
+# will be marked as correct when you submit.
+
+# ENTER YOUR CODE BELOW THIS LINE
+# ===========================================
+
+ggplot(data = diamonds,
+       aes(x = table, y = price)) +
+    geom_point(aes(colour = cut)) +
+    scale_fill_brewer(type = 'qual')
