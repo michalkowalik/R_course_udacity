569eff2db0d352bcb926c26df5a1edb74a8bc1b8
Michal Kowalik <mkowalik@outlook.com>
2016-02-17 23:21:48 +0100

    week 5 problemset finished. rather disappointing.

--- problemSet5.R
+++ problemSet5.R
@@ -43,3 +43,145 @@ ggplot(data = diamonds,
        aes(x = table, y = price)) +
     geom_point(aes(colour = cut)) +
     scale_fill_brewer(type = 'qual')
+
+# Create a scatterplot of diamond price vs.
+# volume (x * y * z) and color the points by
+# the clarity of diamonds. Use scale on the y-axis
+# to take the log10 of price. You should also
+# omit the top 1% of diamond volumes from the plot.
+
+# Note: Volume is a very rough approximation of
+# a diamond's actual volume.
+
+# The plot should look something like this.
+# http://i.imgur.com/excUpea.jpg
+
+# Note: In the link, a color palette of type
+# 'div' was used to color the scatterplot using
+# scale_color_brewer(type = 'div')
+
+# This assignment is not graded and
+# will be marked as correct when you submit.
+
+# ENTER YOUR CODE BELOW THIS LINE
+# ===========================================
+
+diamonds$volume = diamonds$x * diamonds$y * diamonds$z
+
+ggplot(data = diamonds,
+       aes(x = volume, y = price)) +
+    geom_point(aes(color = cut)) +
+    scale_color_brewer(type = 'div') +
+    scale_y_log10() +
+    scale_x_continuous(limits = c(0, 500))
+
+# Many interesting variables are derived from two or more others.
+# For example, we might wonder how much of a person's network on
+# a service like Facebook the user actively initiated. Two users
+# with the same degree (or number of friends) might be very
+# different if one initiated most of those connections on the
+# service, while the other initiated very few. So it could be
+# useful to consider this proportion of existing friendships that
+# the user initiated. This might be a good predictor of how active
+# a user is compared with their peers, or other traits, such as
+# personality (i.e., is this person an extrovert?).
+
+# Your task is to create a new variable called 'prop_initiated'
+# in the Pseudo-Facebook data set. The variable should contain
+# the proportion of friendships that the user initiated.
+
+# This programming assignment WILL BE automatically graded.
+
+pf <- read.delim('pseudo_facebook.tsv')
+pf$prop_initiated <- pf$friendships_initiated / pf$friend_count
+
+# Create a line graph of the median proportion of
+# friendships initiated ('prop_initiated') vs.
+# tenure and color the line segment by
+# year_joined.bucket.
+
+# Recall, we created year_joined.bucket in Lesson 5
+# by first creating year_joined from the variable tenure.
+# Then, we used the cut function on year_joined to create
+# four bins or cohorts of users.
+
+# (2004, 2009]
+# (2009, 2011]
+# (2011, 2012]
+# (2012, 2014]
+
+# The plot should look something like this.
+# http://i.imgur.com/vNjPtDh.jpg
+# OR this
+# http://i.imgur.com/IBN1ufQ.jpg
+
+pf$year_joined =  2014 - ceiling(pf$tenure / 365)
+table(pf$year_joined)
+pf$year_joined.bucket <- cut(pf$year_joined, breaks = c(2004, 2009, 2011, 2012, 2014))
+
+ggplot(aes(x = tenure, y = prop_initiated),
+       data = subset(pf, !is.na(year_joined.bucket))) +
+  geom_line(aes(color = year_joined.bucket), stat = 'summary', fun.y = median)
+
+
+# Smooth the last plot you created of
+# of prop_initiated vs tenure colored by
+# year_joined.bucket. You can bin together ranges
+# of tenure or add a smoother to the plot.
+
+# There won't be a solution image for this exercise.
+# You will answer some questions about your plot in
+# the next two exercises.
+
+# This assignment is not graded and
+# will be marked as correct when you submit.
+
+# ENTER YOUR CODE BELOW THIS LINE
+# ====================================================
+ggplot(aes(x = tenure, y = prop_initiated),
+       data = subset(pf, !is.na(year_joined.bucket))) +
+  geom_line(aes(color = year_joined.bucket),
+            stat = 'summary',
+            fun.y = median) +
+    geom_smooth(aes(color = year_joined.bucket))
+summary(pf$year_joined.bucket)
+
+## for the group with the largets proportion of frienships initiated, what's the
+## group's average (mean) proportion of friendship initiated
+
+library(dplyr)
+
+pf_by_year_joined_bucket <- group_by(subset(pf, !is.na(prop_initiated)),
+                                     year_joined.bucket)
+
+
+res1 <- summarise(pf_by_year_joined_bucket,
+                  mean1 = mean(prop_initiated),
+                  n = n())
+
+print(res1)
+
+
+# Create a scatter plot of the price/carat ratio
+# of diamonds. The variable x should be
+# assigned to cut. The points should be colored
+# by diamond color, and the plot should be
+# faceted by clarity.
+
+# The plot should look something like this.
+# http://i.imgur.com/YzbWkHT.jpg.
+
+# Note: In the link, a color palette of type
+# 'div' was used to color the histogram using
+# scale_color_brewer(type = 'div')
+
+# This assignment is not graded and
+# will be marked as correct when you submit.
+
+# ENTER YOUR CODE BELOW THIS LINE
+# ===========================================
+ggplot(data = diamonds,
+       aes(x = cut, y = price / carat)) +
+    geom_point(aes(color = color)) +
+    scale_color_brewer(type = 'div') +
+    facet_wrap(~ clarity)
